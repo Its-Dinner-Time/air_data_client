49541a0df07e4f32f78e480c606bea22acc6e9c8
dinnerTime <dinnerTime.dev@gmail.com>
2023-04-23 18:42:13 +0900

    백엔드 적용

--- src/apis/air.api.ts
+++ src/apis/air.api.ts
@@ -2,17 +2,8 @@ import axios from 'axios';
 import type { CITIES, IAIR_API_RESULT } from './air.t';
 
 export const getAirApi = async (city: CITIES): Promise<IAIR_API_RESULT[]> => {
-  const params = {
-    serviceKey: '',
-    returnType: 'json',
-    numOfRows: 100,
-    pageNo: 1,
-    sidoName: city,
-    ver: '1.0',
-  };
+  const response = await axios.get('https://dt-air-data.herokuapp.com/api/air-data', { params: { city } });
 
-  const response = await axios.get('', { params });
-
-  const items: IAIR_API_RESULT[] = response.data.response.body.items;
+  const items: IAIR_API_RESULT[] = response.data;
   return items;
 };
